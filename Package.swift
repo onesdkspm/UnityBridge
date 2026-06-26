52ac8c2a26bef45bd43be58c6061fc216627b732
m2构造机 <zhanggaoqi@aobi.com>
2026-06-26 17:57:15 +0800

    [Branch Release] v1.1.5-dev-1473129 - develop
    - 更新所有 XCFrameworks 到 1.1.5-dev-1473129
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "UnityBridge",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBridge/1.1.5-dev-1472019/UnityBridge.xcframework.zip",
-            checksum: "cbf69c3013ec0c521279687d2426499753d12c47d26b8099f1c1ef956ab9d791"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBridge/1.1.5-dev-1473129/UnityBridge.xcframework.zip",
+            checksum: "229f71fa679db443d7ab8b0a7a9ad41953083ef8d5df016b00de020504a3b3c9"
         )
     ]
 )
