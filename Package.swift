008de1707e0538e55f52343e8acaf8b559867229
m2构造机 <zhanggaoqi@aobi.com>
2026-07-07 17:36:31 +0800

    Release v2.0.2
    - 更新所有 XCFrameworks 到 2.0.2
    - 更新资源文件
    - 更新 checksums
    - 自动生成 Info.plist
    - 修复资源路径配置

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "UnityBridge",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBridge/2.0.0/UnityBridge.xcframework.zip",
-            checksum: "ba34569713b091c8e3a6aa50830012dbbe753e3fa495efc6779b767d837a889c"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/UnityBridge/2.0.2/UnityBridge.xcframework.zip",
+            checksum: "003d6216f4c15c6b5e3def839cc0ae6eb4dcc88790216afbd98238131b2c1bcc"
         )
     ]
 )
